a72fb99a6d680f957418c30c344c7b8dd115bd90
Gang <poiut5073@gmail.com>
2021-03-18 13:18:43 +0900

    java

---
+++ ex4/struct/Covid19.java
@@ -0,0 +1,12 @@
+package ex4.struct;
+
+public class Covid19 {
+	String date;
+	int positive;
+	int deaths;
+	int test;
+	
+	
+	
+	
+}
---
+++ ex4/struct/Program.java
@@ -0,0 +1,20 @@
+package ex4.struct;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		String[] row1 =new String[3];
+		row1[2] = "30";
+		row1[1] = "30";
+		
+		Covid19 row2 = new Covid19();
+		row2.deaths =  30;
+		row2.positive = 30;
+		row2.test = 30;
+			
+
+	}
+	
+}
+ 
